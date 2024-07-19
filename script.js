f99a611039ecaea258d46710411a94ea92c93434
krzysztoffila <umowy.fila@gmail.com>
2024-07-19 12:19:18 +0200

    Feature: Revealing Elements on Scroll

--- script.js
+++ script.js
@@ -124,3 +124,20 @@ const headerObsrever = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObsrever.observe(header);
+
+// Revealing sections
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
