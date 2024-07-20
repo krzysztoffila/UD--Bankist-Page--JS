621839a810a468e101148df2e8f2f3a0fa37e338
krzysztoffila <umowy.fila@gmail.com>
2024-07-20 08:58:26 +0200

    testing events

--- tests.js
+++ tests.js
@@ -5,3 +5,15 @@ const logo = document.querySelector(".nav__logo");
 logo.setAttribute("company", "Bankist");
 // console.log(logo.src);
 // console.log(logo.getAttribute("src"));
+document.addEventListener("DOMContentLoaded", function (e) {
+  console.log("HTML Parsed and DOM tree build", e);
+});
+window.addEventListener("load", function (e) {
+  console.log("HTML and CSS loaded", e);
+});
+window.addEventListener("beforeunload", function (e) {
+  e.preventDefault();
+  console.log("Before exit tab", e);
+  e.returnValue = "";
+  //  e.BeforeUnloadEvent = "";
+});
